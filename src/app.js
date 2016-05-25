3dad32eca76e870d9b265feb6cd6bf6a60558e63
Matt Hussey <matthew.hussey@googlemail.com>
2016-05-25 21:29:40 +0100

    >> Move app.js from static to src.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var angular = require('angular');
+
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  entry: './static/app.js',
+  entry: './src/app.js',
   output: {
     path: __dirname + '/app',
     filename: 'bundle.js'
